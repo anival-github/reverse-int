913dbdefe95629cdb9c064ed2efd407721ee865b
Valentin Anikin <anival-94@mail.ru>
2020-10-16 22:03:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let k;
+    if (n < 0) {
+        k = +((-n).toString().split('').reverse().join(''));
+    } else {
+        k = +(n.toString().split('').reverse().join(''));
+    }
+    return k;
 }
